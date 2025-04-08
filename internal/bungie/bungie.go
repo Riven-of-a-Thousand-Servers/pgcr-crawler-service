627d9c35a8546d837fb4fe4d58bf172a3ee9c269
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2025-04-08 01:29:00 -0700

    Still trying to fix the segmentation fault

--- internal/bungie/bungie.go
+++ internal/bungie/bungie.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	types "github.com/Riven-of-a-Thousand-Servers/rivenbot-commons/pkg/types"
@@ -44,7 +45,8 @@ func NewBungieClient(apiKey, host string) (*BungieClient, error) {
 
 func (p *BungieClient) FetchPgcr(instanceId int64) (*types.PostGameCarnageReportResponse, error) {
 	request, err := http.NewRequest("GET", fmt.Sprintf(statsURI, p.Host, instanceId), nil)
-	request.Header.Add(apiKeyHeader, p.ApiKey)
+	log.Printf("BungieClient: %#v", p)
+	request.Header.Set(apiKeyHeader, p.ApiKey)
 
 	response, err := p.Client.Do(request)
 	if err != nil {
