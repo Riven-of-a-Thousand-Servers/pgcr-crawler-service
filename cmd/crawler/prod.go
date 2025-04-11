e7f30549a2b5d6ea98a11323d13081a01a61165e
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2025-04-10 23:22:49 -0700

    Added a retry/skip for checking the proxy health

--- cmd/crawler/prod.go
+++ cmd/crawler/prod.go
@@ -3,51 +3,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"pgcr-crawler-service/internal/bungie"
 	"pgcr-crawler-service/internal/rabbitmq"
 	"pgcr-crawler-service/internal/worker"
 	"sync"
+	"time"
 
 	"github.com/Riven-of-a-Thousand-Servers/rivenbot-commons/pkg/utils"
 	"github.com/rabbitmq/amqp091-go"
 )
 
+func checkProxyHealth(proxyHost string) error {
+	url := fmt.Sprintf("http://%s/healthcheck", proxyHost)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("Failed to reach proxy healthcheck endpoint: %v", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Healthcheck failed with status: %s", resp.Status)
+	}
+
+	return nil
+}
+
 func run() {
-	apiKeyFile := os.Getenv("BUNGIE_API_KEY_FILE")
-	if apiKeyFile == "" {
-		log.Fatal("Bungie api key not found")
-	}
-
-	apiKey, err := utils.ReadSecret(apiKeyFile)
-	if err != nil {
-		log.Fatalf("Error reading secret from file path [%s]: %v", apiKeyFile, err)
-	}
-
-	hostPath := os.Getenv("BUNGIE_HOST_FILE")
-	if hostPath == "" {
-		log.Fatalf("Unable to host path for crawler")
-	}
-
-	host, err := utils.ReadSecret(hostPath)
-	if err != nil {
-		log.Fatalf("Unable to fetch secret with path [%s]: %v", hostPath, err)
-	}
-
+	apiKey, host, rabbitmqUrl := parseEnv()
 	bungieClient, err := bungie.NewBungieClient(apiKey, host)
 	if err != nil {
 		log.Fatalf("Unable to instantiate a bungie client: %v", err)
-	}
-
-	rabbitmqPath := os.Getenv("RABBITMQ_URL_FILE")
-	if rabbitmqPath == "" {
-		log.Fatal("Rabbitmq url not found")
-	}
-
-	rabbitmqUrl, err := utils.ReadSecret(rabbitmqPath)
-	if err != nil {
-		log.Fatalf("Error reading rabbitmq url from path [%s]: %v", rabbitmqUrl, err)
 	}
 
 	conn, err := amqp091.Dial(rabbitmqUrl)
@@ -69,13 +58,17 @@ func run() {
 		log.Fatalf("Unable to instantiate rabbitmq: %v", err)
 	}
 
-	var waitgroup sync.WaitGroup
+	crawl(bungieClient, rabbitmq, host)
+}
 
+func crawl(bungieClient bungie.PgcrClient, rabbitmq rabbitmq.PgcrPublisher, proxyHost string) {
+	waitForHealthyProxy(proxyHost)
+	var waitgroup sync.WaitGroup
 	for i := 0; i < *passes; i++ {
 		ids := make(chan int64, 50)
 		for i := 0; i <= *goroutines; i++ {
 			waitgroup.Add(1)
-			go func(wg *sync.WaitGroup, ids chan int64) {
+			go func(ids chan int64) {
 				defer waitgroup.Done()
 				worker := worker.NewWorker(bungieClient, rabbitmq)
 
@@ -87,11 +80,11 @@ func run() {
 					log.Printf("Working on pgcr [%d]...", id)
 					err := worker.Work(id)
 					if err != nil {
-						log.Printf("Received error for pgcr [%d]: %w", id, err)
+						log.Printf("Received error for pgcr [%d]: %v", id, err)
 					}
 					log.Printf("Finished pgcr [%d]", id)
 				}
-			}(&waitgroup, ids)
+			}(ids)
 		}
 
 		for i := 0; i < int(*period); i++ {
@@ -101,4 +94,58 @@ func run() {
 		close(ids)
 		waitgroup.Wait()
 	}
+}
+
+func waitForHealthyProxy(proxyHost string) {
+	healthy := false
+	maxAttempts := 5
+	for i := 0; i < maxAttempts; i++ {
+		err := checkProxyHealth(proxyHost)
+		if err != nil {
+			log.Printf("Attempt %d/%d: Proxy health check failed : %v", i+1, maxAttempts, err)
+			time.Sleep(15 * time.Second)
+			continue
+		}
+		log.Println("Proxy is healthy, starting the crawler...")
+		healthy = true
+		break
+	}
+
+	if !healthy {
+		log.Fatalf("Proxy did not become healthy after 5 attemps. Exiting")
+	}
+}
+
+func parseEnv() (apiKey, proxyHost, rabbitmqUrl string) {
+	apiKeyFile := os.Getenv("BUNGIE_API_KEY_FILE")
+	if apiKeyFile == "" {
+		log.Fatal("Bungie api key not found")
+	}
+
+	apiKey, err := utils.ReadSecret(apiKeyFile)
+	if err != nil {
+		log.Fatalf("Error reading secret from file path [%s]: %v", apiKeyFile, err)
+	}
+
+	hostPath := os.Getenv("BUNGIE_HOST_FILE")
+	if hostPath == "" {
+		log.Fatalf("Unable to host path for crawler")
+	}
+
+	proxyHost, err = utils.ReadSecret(hostPath)
+	if err != nil {
+		log.Fatalf("Unable to fetch secret with path [%s]: %v", hostPath, err)
+	}
+
+	rabbitmqPath := os.Getenv("RABBITMQ_URL_FILE")
+	if rabbitmqPath == "" {
+		log.Fatal("Rabbitmq url not found")
+	}
+
+	rabbitmqUrl, err = utils.ReadSecret(rabbitmqPath)
+	if err != nil {
+		log.Fatalf("Error reading rabbitmq url from path [%s]: %v", rabbitmqUrl, err)
+	}
+
+	return apiKey, proxyHost, rabbitmqUrl
 }
